ock-php/ock-mono

Register class as public service if it has #[Service] attribute.

packages/dependency-injection/tests/src/Fixtures/ServiceAttributeOnClass/ServiceWithId.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttributeOnClass;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service(serviceId: 'custom_service_id')]
+class ServiceWithId {
+
+}

packages/dependency-injection/tests/src/Fixtures/ServiceAttributeOnClass/ServiceWithSuffix.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttributeOnClass;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service(serviceIdSuffix: 'customSuffix')]
+class ServiceWithSuffix {
+
+}

packages/dependency-injection/tests/src/Fixtures/ServiceAttributeOnClass/ServiceWithTarget.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttributeOnClass;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service(target: 'customTarget')]
+class ServiceWithTarget {
+
+}

packages/dependency-injection/tests/src/Fixtures/ServiceAttributeOnClass/SimplePublicService.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttributeOnClass;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service]
+class SimplePublicService {
+
+}

ock-php/dependency-injection

tests/src/Fixtures/ServiceAttribute/ServiceWithId.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttribute;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service(serviceId: 'custom_service_id')]
+class ServiceWithId {
+
+}

tests/src/Fixtures/ServiceAttribute/ServiceWithSuffix.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttribute;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service(serviceIdSuffix: 'customSuffix')]
+class ServiceWithSuffix {
+
+}

tests/src/Fixtures/ServiceAttribute/ServiceWithTarget.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttribute;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service(target: 'customTarget')]
+class ServiceWithTarget {
+
+}

tests/src/Fixtures/ServiceAttribute/SimplePublicService.php
@@ -1,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttribute;
+
+use Ock\DependencyInjection\Attribute\Service;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+#[Service]
+class SimplePublicService {
+
+}